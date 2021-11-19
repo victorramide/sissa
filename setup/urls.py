b344619421d9b82c2dd026fdef078c02044e3767
Victor Ramide da Costa Souza <victorramide@gmail.com>
2021-11-19 12:24:52 -0300

    Refatoração do projeto

--- setup/urls.py
+++ setup/urls.py
@@ -1,4 +1,4 @@
-"""sissa URL Configuration
+"""setup URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
