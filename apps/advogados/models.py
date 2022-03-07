70028cfd5fd4bd6f832f6d42a41f9a62b08774f8
victorramide <victorramide@gmail.com>
2022-03-06 21:04:37 -0300

    Melhorias na apresentação das informações

--- apps/advogados/models.py
+++ apps/advogados/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 # Create your models here.
 class Advogado(models.Model):
-    user = models.OneToOneField(User, related_name='advogado', on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, related_name='advogado', on_delete=models.CASCADE)
     oab = models.CharField(max_length=10)
     uf = models.CharField(max_length=2)
 
     def __str__(self):
-        return self.user.username+" - "+self.oab+"/"+self.uf
+        return f'{self.user.first_name} {self.user.last_name} - {self.oab}/{self.uf}'
